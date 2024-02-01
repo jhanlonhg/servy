83d2e4a717316f288d0183cfcc48cb8665765b22
John Hanlon <john.hanlon@hginsights.com>
2024-02-01 11:25:46 -0800

    Refactor portions of the handler module into their own modules End of lesson

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,8 +2,12 @@ defmodule Servy.Handler do
   @moduledoc """
     Handles HTTP requests
   """
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
 
   @pages_path Path.expand("../../pages", __DIR__)
+
   @doc """
     Transforms a request into a response
   """
@@ -16,36 +20,6 @@ defmodule Servy.Handler do
     |> track()
     |> format_response()
   end
-
-  def track(%{ status: 404, path: path } = conv) do
-    IO.puts "Warning: #{path} is on the loose!"
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{ path: "/wildlife" } = conv) do
-    %{ conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def parse(request) do
-    [method, path, _] =
-      request
-        |> String.split("\n")
-        |> List.first()
-        |> String.split(" ")
-
-    %{
-      method: method,
-      path: path,
-      resp_body: "",
-      status: nil
-    }
-  end
-
-  def log(conv), do: IO.inspect conv
 
   def route(%{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
@@ -70,11 +44,7 @@ defmodule Servy.Handler do
     %{ conv | status: 404, resp_body: "No #{path} found!"}
   end
 
-  defp handle_file({:ok, content}, conv), do: %{ conv | status: 200, resp_body: content }
 
-  defp handle_file({:error, :enoent}, conv), do: %{ conv | status: 404, resp_body: "File not found!" }
-
-  defp handle_file({:error, reason}, conv), do: %{conv | status: 500, resp_body: "File error: #{reason}"}
 
   def format_response(conv) do
     """
